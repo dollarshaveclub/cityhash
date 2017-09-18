641a64391b3e565f84bcb1bbd78060defa49e74f
David Huie <dahuie@gmail.com>
2017-09-18 12:01:15 -0700

    Replace StringValuePtr with StringValueCStr
    The function StringValuePtr does not return null char terminated
    strings, whereas StringValueCStr does. We found that this can cause
    segementation faults on certain systems.

--- ext/cityhash/cityhash.cc
+++ ext/cityhash/cityhash.cc
@@ -11,31 +11,31 @@ typedef VALUE (ruby_method)(...);
 
 extern "C" VALUE cityhash_hash32(VALUE mod, VALUE input)
 {
-  return ULL2NUM(CityHash32(StringValuePtr(input), RSTRING_LEN(input)));
+  return ULL2NUM(CityHash32(StringValueCStr(input), RSTRING_LEN(input)));
 }
 
 extern "C" VALUE cityhash_hash64(VALUE mod, VALUE input)
 {
   Check_Type(input, T_STRING);
-  return ULL2NUM(CityHash64(StringValuePtr(input), RSTRING_LEN(input)));
+  return ULL2NUM(CityHash64(StringValueCStr(input), RSTRING_LEN(input)));
 }
 
 extern "C" VALUE cityhash_hash64_with_seed(VALUE mod, VALUE input, VALUE seed)
 {
   Check_Type(input, T_STRING);
-  return ULL2NUM(CityHash64WithSeed(StringValuePtr(input), RSTRING_LEN(input), NUM2ULL(seed)));
+  return ULL2NUM(CityHash64WithSeed(StringValueCStr(input), RSTRING_LEN(input), NUM2ULL(seed)));
 }
 
 extern "C" VALUE cityhash_hash64_with_seeds(VALUE mod, VALUE input, VALUE seed1, VALUE seed2)
 {
   Check_Type(input, T_STRING);
-  return ULL2NUM(CityHash64WithSeeds(StringValuePtr(input), RSTRING_LEN(input), NUM2ULL(seed1), NUM2ULL(seed2)));
+  return ULL2NUM(CityHash64WithSeeds(StringValueCStr(input), RSTRING_LEN(input), NUM2ULL(seed1), NUM2ULL(seed2)));
 }
 
 extern "C" VALUE cityhash_hash128(VALUE mod, VALUE input)
 {
   Check_Type(input, T_STRING);
-  uint128 hash = CityHash128(StringValuePtr(input), RSTRING_LEN(input));
+  uint128 hash = CityHash128(StringValueCStr(input), RSTRING_LEN(input));
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 
@@ -43,28 +43,28 @@ extern "C" VALUE cityhash_hash128_with_seed(VALUE mod, VALUE input, VALUE seed_s
 {
   Check_Type(input, T_STRING);
   uint128 seed = *(uint128 *)StringValuePtr(seed_string);
-  uint128 hash = CityHash128WithSeed(StringValuePtr(input), RSTRING_LEN(input), seed);
+  uint128 hash = CityHash128WithSeed(StringValueCStr(input), RSTRING_LEN(input), seed);
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 
 #ifdef __SSE4_2__
 extern "C" VALUE cityhash_hashcrc128(VALUE mod, VALUE input)
 {
-  uint128 hash = CityHashCrc128(StringValuePtr(input), RSTRING_LEN(input));
+  uint128 hash = CityHashCrc128(StringValueCStr(input), RSTRING_LEN(input));
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 
 extern "C" VALUE cityhash_hashcrc128_with_seed(VALUE mod, VALUE input, VALUE seed_string)
 {
   uint128 seed = *(uint128 *)StringValuePtr(seed_string);
-  uint128 hash = CityHashCrc128WithSeed(StringValuePtr(input), RSTRING_LEN(input), seed);
+  uint128 hash = CityHashCrc128WithSeed(StringValueCStr(input), RSTRING_LEN(input), seed);
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 
 extern "C" VALUE cityhash_hashcrc256(VALUE mod, VALUE input)
 {
   uint64 hash[4] = {};
-  CityHashCrc256(StringValuePtr(input), RSTRING_LEN(input), hash);
+  CityHashCrc256(StringValueCStr(input), RSTRING_LEN(input), hash);
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 #endif
